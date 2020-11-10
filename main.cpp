dce8563d58f4c2926b570b725fa112e6f95f527c
Ana <ana.lau.villarreal@gmail.com>
2020-11-10 20:30:43 -0300

    agregue option

--- main.cpp
+++ main.cpp
@@ -11,17 +11,51 @@ int main()
 {
      setlocale(LC_ALL,"Spanish");
 
-    cls();
-    setColor(WHITE);
-    cout << "BRIAN LARA'S FISHING TOURNAMENT "<< endl;
-    cout << "--------------------------------"<< endl;
-    cout << "1 - REGISTRAR CAPTURAS"<< endl;
-    cout << "2 - REPORTE A"<< endl;
-    cout << "3 - REPORTE B"<< endl;
-    cout << "4 - REPORTE C"<< endl;
-    cout << "5 - CRÉDITOS"<< endl;
-    cout << "--------------------------------"<< endl;
-    cout << "0 - SALIR"<< endl;
+     int opcion;
+
+    do{
+        cls();
+        setColor(WHITE);
+        cout << "BRIAN LARA'S FISHING TOURNAMENT "<< endl;
+        cout << "--------------------------------"<< endl;
+        cout << "1 - REGISTRAR CAPTURAS"<< endl;
+        cout << "2 - REPORTE A"<< endl;
+        cout << "3 - REPORTE B"<< endl;
+        cout << "4 - REPORTE C"<< endl;
+        cout << "5 - CRÉDITOS"<< endl;
+        cout << "--------------------------------"<< endl;
+        cout << "0 - SALIR"<< endl;
+        cout << endl; << endl << " < ";
+        //cin >> opcion;
+        opcion = get.key();
+
+        switch(opcion){
+            case 49:
+                count << "Opción 1";
+            break;
+            case 50:
+                count << "Opción 2";
+            break;
+            case 51:
+                count << "Opción 3";
+            break;
+            case 52:
+                count << "Opción 4";
+            break;
+            case 53:
+                count << "Opción 5;
+            break;
+            case 48:
+
+            break;
+
+        }
+            //cin.ignore();
+            anykey();
+
+    }while(opcion != 48 );
+
+
 
 
 
