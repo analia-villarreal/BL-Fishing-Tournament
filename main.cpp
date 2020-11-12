10975d1e0f1f243702b0e4deab586b4a48bac0ef
Javier Bareiro <j.bareiro1993@yahoo.com.ar>
2020-11-11 21:52:47 -0300

    13/11

--- main.cpp
+++ main.cpp
@@ -3,24 +3,30 @@ using namespace std;
 #include <ctime>
 #include <cstdlib>
 #include <clocale>
-#include "Bibliotecas/rlutil.h"
+#include "rlutil.h"
 #include "funciones.h"
-using namespace rlutil.h;
+using namespace rlutil;
+int main()
+{
+      setlocale(LC_ALL,"Spanish");
 
-int main(){
-     setlocale(LC_ALL,"Spanish");
-
-     int opcion;
+     int opcion, i;
      const int t1=15;
      const int t2=9;
-
-     int participantes[15];
+    int vecparticipante[]={};
+    int veccodigo[t1]={100,101,102,103,104,105,106,107,108,109,110,111,112,113,114};
+    float  veckg[]={};
+    int vechora[]={};
+     float participantes[]={};
      float peso_minimo[t2]={0.5, 2, 4, 5, 1, 5, 1.5, 3, 2.5 };
      string especie[t2]={"Anchoa","Pejerrey","Bagre","Boga","Caballa","Carpa","Trucha patagónica","Dorado","Lisa"};
 
+    ponerceroint(veccodigo, 15);
+
     do{
-      //  cls();
-        //setColor(WHITE);
+       /// setBackgroundColor (BLUE);
+        cls();
+        setColor(WHITE);
         cout << "BRIAN LARA'S FISHING TOURNAMENT "<< endl;
         cout << "--------------------------------"<< endl;
         cout << "1 - REGISTRAR CAPTURAS"<< endl;
@@ -30,39 +36,45 @@ int main(){
         cout << "5 - CRÉDITOS"<< endl;
         cout << "--------------------------------"<< endl;
         cout << "0 - SALIR"<< endl;
-        cout << endl << endl << " < ";
-        //cin >> opcion;
-        opcion = get.key();
-
+        cout << endl << endl << " > ";
+       // cin >> opcion;
+        opcion = getkey();
+     //   cout << "Opción elegida " << opcion << endl;
         switch(opcion){
             case 49:
-                count << "Opción 1";
+                ingresocaptura (veccodigo, vechora, participantes, peso_minimo);
             break;
             case 50:
-                count << "Opción 2";
+
+                puntoa (veccodigo, participantes);
+                //for (i=0; i<15; i++){
+                cout << participantes[0] << endl;
+                cout << veccodigo[0] << endl;
+               // }
+           // Reporte A - Ganador de la competencia
+           // Listar por pantalla el código del pescador/a que vaya ganando la competencia
+           // y la totalidad de kilogramos capturados hasta el momento.
+
+          ///      cout << ;
             break;
             case 51:
-                count << "Opción 3";
+                cout << "Opción 3";
             break;
             case 52:
-                count << "Opción 4";
+                cout << "Opción 4";
             break;
             case 53:
-                count << "Opción 5";
+                cout << "Opción 5";
             break;
             case 48:
-
+              ///  return 0;
             break;
 
         }
-            //cin.ignore();
-            anykey();
+      //   cin.ignore();
+          anykey();
 
     }while(opcion != 48 );
-
-
-
-
 
     return 0;
 }
